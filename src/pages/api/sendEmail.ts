f90f4ecd1197a022e26344770cbfbad7d3ec2a47
Abe Choi <abraham.choi@icloud.com>
2023-03-18 20:43:04 -0700

    added dependencies and variable type check

--- src/pages/api/sendEmail.ts
+++ src/pages/api/sendEmail.ts
@@ -2,6 +2,10 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import sgMail from '@sendgrid/mail';
 
+if (!process.env.SENDGRID_API_KEY) {
+  throw new Error('SENDGRID_API_KEY is not defined');
+}
+
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
