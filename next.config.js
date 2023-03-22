45c7bfc73a1e2c61b9cd8daca582084a099d77a8
Abe Choi <abraham.choi@icloud.com>
2023-03-21 19:51:48 -0700

    added NEXT_PUBLIC

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
-
 // next.config.js
 const { parsed: localEnv } = require('dotenv').config();
 
 module.exports = {
+  reactStrictMode: true,
   env: {
+    // Add any environment variables you want to expose to the frontend here
+    NEXT_PUBLIC_WORKMAIL_EMAIL_ADDRESS: process.env.WORKMAIL_EMAIL_ADDRESS,
+    NEXT_PUBLIC_ACCESS_KEY_ID: process.env.ACCESS_KEY_ID,
+    NEXT_PUBLIC_SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY,
+    NEXT_PUBLIC_REGION: process.env.REGION,
   },
 };
